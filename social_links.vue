66830a6f359886d89b37e5c39380cc4c56519bb4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-07 16:51:42 -0500

    Bramalea City Centre 1.0  /social_links.vue

--- social_links.vue
+++ social_links.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-        
-    </div>
-    <a href="https://www.instagram.com/shopbdc_" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+        <a href="https://www.instagram.com/shopbdc_" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
     <a href="https://www.facebook.com/BramaleaCityCentre" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
     <a href="https://www.pinterest.ca/bonniedoonsc" target="_blank"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
     <a href="https://www.twitter.com/bonniedoonsc" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+    </div>
+    
 </template>
 <script>
     define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment"], function(Vue, Vuex, moment, tz, VueMoment) {
