3e323a15799311b23193cebe031568a067a6e55d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-07 16:49:35 -0500

    Bramalea City Centre 1.0  /social_links.vue

--- social_links.vue
+++ social_links.vue
@@ -8,32 +8,32 @@
     define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment"], function(Vue, Vuex, moment, tz, VueMoment) {
         return Vue.component("social-links-component", {
             template: template, // the variable template will be injected,
-            data: function() {
-                return {
-                    success_subscribe: false,
-                    currentPage: null,
-                    pageBanner : null
-                }
-            },
-            computed: {
-                ...Vuex.mapGetters([
-                    'property',
-                    'timezone',
-                    'findRepoByName'
-                ])
-            },
-            methods: {
-                loadData: async function(id) {
-                    try {
-                        // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
-                        console.log("this.id", this.id);
-                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/" + id + ".json"}),this.$store.dispatch("getData", "repos")]);
-                        return results;
-                    } catch (e) {
-                        console.log("Error loading data: " + e.message);
-                    }
-                },
-            }
+            // data: function() {
+            //     return {
+            //         success_subscribe: false,
+            //         currentPage: null,
+            //         pageBanner : null
+            //     }
+            // },
+            // computed: {
+            //     ...Vuex.mapGetters([
+            //         'property',
+            //         'timezone',
+            //         'findRepoByName'
+            //     ])
+            // },
+            // methods: {
+            //     loadData: async function(id) {
+            //         try {
+            //             // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
+            //             console.log("this.id", this.id);
+            //             let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/" + id + ".json"}),this.$store.dispatch("getData", "repos")]);
+            //             return results;
+            //         } catch (e) {
+            //             console.log("Error loading data: " + e.message);
+            //         }
+            //     },
+            // }
         });
     });
 </script>
