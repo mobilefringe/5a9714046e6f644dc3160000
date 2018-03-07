d4f2f735ac4a4bf3c0e1f7ea2ff78a3559b1825c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-07 16:21:37 -0500

    Bramalea City Centre 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -173,7 +173,7 @@
                         }
                         else if (value.store == null || value.store == undefined) {
                             value.store = {};
-                            value.store.image_url =  "//codecloud.cdn.speedyrails.net/sites/5a6a54eb6e6f647da51e0100/image/png/1518554684072/bonniedoonlogo.png";
+                            value.store.image_url =  this.property.default_logo_url;
                         }
                         if (_.includes(value.image_url, 'missing')) {
                             value.image_url = "http://via.placeholder.com/400x400/757575";
