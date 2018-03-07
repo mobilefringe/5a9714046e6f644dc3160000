828a5bbd4c60d6f2d1d8bffe3cba72ef2b5171b8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-07 16:51:39 -0500

    Bramalea City Centre 1.0  /social_links.vue

--- social_links.vue
+++ social_links.vue
@@ -1,4 +1,7 @@
 <template>
+    <div>
+        
+    </div>
     <a href="https://www.instagram.com/shopbdc_" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
     <a href="https://www.facebook.com/BramaleaCityCentre" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
     <a href="https://www.pinterest.ca/bonniedoonsc" target="_blank"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
