1b68e005cb41ab729c361a11137cf849e9166c38
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-08 17:29:54 -0500

    Bramalea City Centre 1.0  /social_links.vue

--- social_links.vue
+++ social_links.vue
@@ -16,32 +16,7 @@
         Vue.use(Lightbox);
         return Vue.component("social-links", {
             template: template, // the variable template will be injected,
-            // data: function() {
-            //     return {
-            //         success_subscribe: false,
-            //         currentPage: null,
-            //         pageBanner : null
-            //     }
-            // },
-            // computed: {
-            //     ...Vuex.mapGetters([
-            //         'property',
-            //         'timezone',
-            //         'findRepoByName'
-            //     ])
-            // },
-            // methods: {
-            //     loadData: async function(id) {
-            //         try {
-            //             // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
-            //             console.log("this.id", this.id);
-            //             let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/" + id + ".json"}),this.$store.dispatch("getData", "repos")]);
-            //             return results;
-            //         } catch (e) {
-            //             console.log("Error loading data: " + e.message);
-            //         }
-            //     },
-            // }
+            
         });
     });
 </script>
