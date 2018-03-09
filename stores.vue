aa03f723da70e23cb3f62c45893e112b2841b626
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-09 10:32:16 -0500

    Bramalea City Centre 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -74,8 +74,6 @@
                 return {
                     listMode: "alphabetical",
                     selectedCat: null,
-                    selectedAlpha: "All",
-                    alphabet: ["All", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
                     filteredStores: null,
                     dataloaded: false,
                     storeBanner : null,
