f6ed6ba670145b8a2ce21dcb22334c5fd14498aa
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-09 12:03:55 -0500

    Bramalea City Centre 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -20,7 +20,7 @@
                         <div class="form-group">
                             <div class="col-sm-6 col-xs-12">
                                 <label class="label" for="cm-fuljdi-fuljdi">{{$t("newsletter_page.email")}}</label>
-                                <input v-model="form_data.email" required class="form-control" name="cm-fuljdi-fuljdi" type="email" placeholder="Email" id="newsletter_email">
+                                <input v-model="form_data.email" required class="form-control" name="cm-fuljdi-fuljdi" type="email" placeholder="Email" id="fieldEmail">
                             </div>
                         </div>
                         <div class="form-group">
