3048214fd0e31ae05cfd30ab317ea2d2c79a1535
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-12 15:17:39 -0400

    Bramalea City Centre 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -107,6 +107,7 @@
 <script>
     define(["Vue", "vuex", 'vue!social_links.vue', "bootstrap-vue"], function (Vue, Vuex, SocialLinks, BootstrapVue) {
         Vue.use(Lightbox);
+        Vue.use(BootstrapVue);
         return Vue.component("social-links", {
             template: template, // the variable template will be injected,
         });
