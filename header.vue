db7e95dc4d3d4b30106429d19291e5b56dfd01a7
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-13 10:58:37 -0400

    Bramalea City Centre 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -97,7 +97,7 @@
 </template>
 
 <script>
-    define(["Vue", "vuex", 'vue!social_links.vue', "bootstrap-vue"], function (Vue, Vuex, SocialLinks, BootstrapVue) {
+    define(["Vue", "vuex", 'vue!social_links.vue', "bootstrap-vue", 'json!menu_items.json'], function (Vue, Vuex, SocialLinks, BootstrapVue) {
         Vue.use(BootstrapVue);
         return Vue.component("header-component", {
             template: template, // the variable template will be injected,
