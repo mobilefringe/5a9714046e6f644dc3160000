f7a6adefa77a31f7bff6dcc878fc1916d84617a9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-23 13:37:57 -0400

    Bramalea City Centre 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -163,8 +163,8 @@
                     var temp_promo = [];
                     _.forEach(this.processedEvents, function(value, key) {
                         today = moment().tz(vm.timezone);
-                        webDate = moment(value.show_on_web_date).tz(vm.timezone)
-                        if (today.format('DMY') >= webDate.format('DMY')) {
+                        webDate = moment(value.show_on_web_date).tz(vm.timezone);
+                        if (today >= webDate) {
                             value.description_short = _.truncate(value.description, {
                                 'length': 150
                             });
