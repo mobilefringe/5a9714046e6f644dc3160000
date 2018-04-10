894dfd7c4c2873fba45f0943f0e1db2b45464b80
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-10 17:32:19 -0400

    Bramalea City Centre 1.0  /job_details.vue

--- job_details.vue
+++ job_details.vue
@@ -130,7 +130,7 @@
                             }
                             storeHours.push();
                         });
-                        this.storeHours = _.sortBy(storeHours, [function(o) { return o.order; }]);;
+                        this.storeHours = _.sortBy(storeHours, [function(o) { return o.order; }]);
                     }
                 }
             },
