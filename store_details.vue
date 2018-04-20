23a605919d94046f2f97ab5601ee933791a8b6fd
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-20 11:59:26 -0400

    Bramalea City Centre 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -34,7 +34,7 @@
 							<li v-if="storeHours" v-for="hour in storeHours" class="col-xs-12">
 							    <span class="col-xs-6 text-left">{{hour.day_of_week | moment("dddd", timezone)}}</span>
 								<span v-if="hour.is_closed" class="col-xs-6 text-left">Closed</span>
-								<span v-else class="col-xs-6 text-left">{{hour.open_time | moment("hmm A", timezone)}} - {{hour.close_time | moment("h A", timezone)}}</span>
+								<span v-else class="col-xs-6 text-left">{{hour.open_time | moment("h:mm A", timezone)}} - {{hour.close_time | moment("h A", timezone)}}</span>
 							</li>
 						</ul>
 					</div>
