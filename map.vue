36699133a7a165e25b019750d5decf4ec0858f4d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-24 10:24:46 -0400

    Bramalea City Centre 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -81,7 +81,6 @@
                     if(temp_repo) {
                         this.pageBanner = temp_repo.images[0];
                     }
-                    console.log(temp_repo, this.pageBanner); 
                     this.$on('updateMap', this.updatePNGMap);
                 });
             },
