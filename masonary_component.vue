4c67ccaa1e0b9eb739b2105e9eecca400ddb679f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-03 17:41:17 -0400

    Bramalea City Centre 1.0  /masonary_component.vue

--- masonary_component.vue
+++ masonary_component.vue
@@ -1,29 +1,29 @@
 <template>
     <div v-masonry transition-duration="0.3s" item-selector=".grid-item" class="visible_phone" >
-                <div v-masonry-tile class="item" >
-                    <div v-for="feature in mobile_feature_items" :class="'grid-item ' + feature.masonry_class ">
-                    	<div  :class="{ 'ih-item circle effect19' : feature.no_hover_class}"> 
-                    	<!--class="ih-item circle effect19"-->
-                    	     <a v-if="feature.do_anchor_tag" :href="feature.url" target="_blank">
-                    			<img :src="feature.image_url" alt="name">
-                    			<div class="info">
-                    				<div class="content">
-                    					<span v-if="feature.name.length > 0"><h3 v-if="locale=='en-ca'">{{feature.name}} </h3></span>
-                    					<span v-if="feature.name_2.length > 0"><h3 v-else> {{feature.name_2}} </h3></span>
-                    				</div>
-                    			</div>
-                    		</a>
-                    		<router-link :to="feature.url" v-else>
-                    			<img :src="feature.image_url" alt="name">
-                    			<div class="info">
-                    				<div class="content">
-                    					<span v-if="feature.name.length > 0"><h3 v-if="locale=='en-ca'">{{feature.name}} </h3></span>
-                    					<span v-if="feature.name_2.length > 0"><h3 v-else> {{feature.name_2}} </h3></span>
-                    				</div>
-                    			</div>
-                    		</router-link>
-                    	</div>
-                    </div>
-                </div>
+        <div v-masonry-tile class="item" >
+            <div v-for="feature in mobile_feature_items" :class="'grid-item ' + feature.masonry_class ">
+            	<div  :class="{ 'ih-item circle effect19' : feature.no_hover_class}"> 
+            	<!--class="ih-item circle effect19"-->
+            	     <a v-if="feature.do_anchor_tag" :href="feature.url" target="_blank">
+            			<img :src="feature.image_url" alt="name">
+            			<div class="info">
+            				<div class="content">
+            					<span v-if="feature.name.length > 0"><h3 v-if="locale=='en-ca'">{{feature.name}} </h3></span>
+            					<span v-if="feature.name_2.length > 0"><h3 v-else> {{feature.name_2}} </h3></span>
+            				</div>
+            			</div>
+            		</a>
+            		<router-link :to="feature.url" v-else>
+            			<img :src="feature.image_url" alt="name">
+            			<div class="info">
+            				<div class="content">
+            					<span v-if="feature.name.length > 0"><h3 v-if="locale=='en-ca'">{{feature.name}} </h3></span>
+            					<span v-if="feature.name_2.length > 0"><h3 v-else> {{feature.name_2}} </h3></span>
+            				</div>
+            			</div>
+            		</router-link>
+            	</div>
             </div>
+        </div>
+    </div>
 </template>
