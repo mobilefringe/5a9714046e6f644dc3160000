2d3c462708f6d2cae51ee1088f1bb833b65d4c14
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-03 17:42:35 -0400

    Bramalea City Centre 1.0  /masonary_component.vue

--- masonary_component.vue
+++ masonary_component.vue
@@ -31,7 +31,7 @@
 <script>
     define(["Vue", "vuex", 'masonry' , 'vue-masonry-plugin'], function(Vue, Vuex, masonry, VueMasonryPlugin) {
         Vue.use(VueMasonryPlugin.default);
-        return Vue.component("social-links", {
+        return Vue.component("masonry-component", {
             template: template, // the variable template will be injected,
         });
     });
