82c443348f4e53dd47010ffb79a7c6692077e828
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-04 12:31:37 -0400

    Bramalea City Centre 1.1  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -5,6 +5,8 @@
 			<div class="site_container">
 				<div class="header_content caps">
 					<h1>{{$t("promos_page.promotions")}}</h1>
+					<h2 style="display:none;"></h2>
+					<h3></h3>
 				</div>
 			</div>
 		</div>
