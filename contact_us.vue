25c235e60fc83afa8812565bd9ca7e16584a3648
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-04 12:55:39 -0400

    Bramalea City Centre 1.1  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -29,7 +29,7 @@
                                 <span v-show="errors.has('email')" class="form-control-feedback">{{ errors.first('email') }}</span>
                             </div>
                             <div class="col-xs-12" :class="{'has-error': errors.has('message')}">
-                                <label class="label" for="message">Message</label>
+                                <label class="label" for="contact_message">Message</label>
                                 <textarea v-model="form_data.message" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="message" type="text" placeholder="Message" data-vv-delay="500"></textarea>
                                 <span v-show="errors.has('message')" class="form-control-feedback">{{ errors.first('message') }}</span>
                             </div>
