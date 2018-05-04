bf056a049618ee1f8f1fa3bd8020b9bdb82ec00a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-04 12:24:36 -0400

    Bramalea City Centre 1.1  /restaurants.vue

--- restaurants.vue
+++ restaurants.vue
@@ -44,17 +44,13 @@
 					<div class="col-xs-6 col-sm-3 col-md-2 cats_row" v-for="store in filteredStores" :data-cat="store.cat_list">
 						<div class="store_logo_container" :id="store.initial">
 							<router-link :to="'/stores/'+ store.slug">
-								<img class="store_img" :style="store.initial_img" :src="store.store_front_url_abs"  :alt="'Click here to view info about ' + store.name +  store.id"/>
+								<img class="store_img" :style="store.initial_img" :src="store.store_front_url_abs" :alt="'Click here to view info about ' + store.name +  store.id"/>
 								<div class="store_coming_soon" v-if="store.is_coming_soon_store">
 									<div class="new_store">{{$t("stores_page.coming_soon")}}</div>
 								</div>
 								<div class="store_coming_soon" v-if="store.is_new_store">
 									<div class="new_store">{{$t("stores_page.new_store")}}</div>
 								</div>
-								<!--<div class="store_list_promos">-->
-								<!--	<span class="promo_exist" v-if="store.total_published_jobs > 0"><img src="//codecloud.cdn.speedyrails.net/sites/58bdb9106e6f644783090000/image/png/1489000358000/jobs.png" class="" alt=""> {{store.total_published_jobs}}</span>-->
-								<!--	<span class="promo_exist" v-if="store.total_published_promos > 0"><img src="//codecloud.cdn.speedyrails.net/sites/58bdb9106e6f644783090000/image/png/1489000371000/promotions.png" class="" alt=""> {{store.total_published_promos}}</span>-->
-								<!--</div>-->
 							</router-link>
 						</div>
 					</div>
