443bbf15802623f6fcee3296a0a49bb7310f02b1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-04 11:26:29 -0400

    Bramalea City Centre 1.1  /stores.vue

--- stores.vue
+++ stores.vue
@@ -52,7 +52,7 @@
 								<div class="store_coming_soon" v-if="store.is_new_store">
 									<div class="new_store">{{$t("stores_page.new_store")}}</div>
 								</div>
-								<h3 style="display:none;">{{store.name}}</h3>
+								<h2 style="display:none;">{{store.name}}</h2>
 							</router-link>
 						</div>
 					</div>
