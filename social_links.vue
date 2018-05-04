33bc7d0a4326ccf819e7f7abdd79c6af97aff682
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-04 09:53:41 -0400

    Bramalea City Centre 1.1  /social_links.vue

---
+++ social_links.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <a href="https://www.facebook.com/BramaleaCityCentre" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i><p style="display:none">Facebook</p></a>
+        <a href="https://www.twitter.com/BCCstyle" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i><p style="display:none">Twitter</p></a>
+        <a href="https://www.instagram.com/bccstyle" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i><p style="display:none">Instagram</p></a>
+        <a href="//codecloud.cdn.speedyrails.net/sites/5aec62a16e6f64422c090000/image/png/1520548054663/bccsnapcode.png" data-lightbox="Snapchat" data-title="BCC Snapchat" alt="BCC Snapchat"> <i class="fa fa-snapchat-ghost"></i><p style="display:none">Snapchat</p></a>
+         
+    </div>
+    
+</template>
+<style>
+    .lb-data .lb-number, .lb-nav {
+        display:none!important;
+    }
+</style>
+<script>
+    define(["Vue", "vuex", "lightbox"], function(Vue, Vuex, Lightbox) {
+        Vue.use(Lightbox);
+        return Vue.component("social-links", {
+            template: template, // the variable template will be injected,
+        });
+    });
+</script>
