09ed365e400284a06abbcab6437975fa0b382b3d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-04 12:38:16 -0400

    Bramalea City Centre 1.1  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -4,7 +4,7 @@
 			<div class="site_container">
 				<div class="header_content caps">
 					<h1>{{$t("events_page.events")}}</h1>
-					<h2 style="display:none;">Scroll to  view jobs</h2>
+					<h2 style="display:none;">Scroll to  view events</h2>
 				</div>
 			</div>
 		</div>
