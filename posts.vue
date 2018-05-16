79eac1dacac7bd069830d7e60b437ccc57806f10
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-16 10:04:56 -0400

    Bramalea City Centre 1.2  /posts.vue

--- posts.vue
+++ posts.vue
@@ -5,10 +5,10 @@
 			<!--http://via.placeholder.com/1920x300-->
 			<div class="site_container">
 				<div class="header_content">
-					<h1 v-if="locale=='en-ca'">{{currentPage.title}}</h1>
-					<h1 v-else>{{currentPage.title_2}}</h1>
-					<h2 style="display:none;">Scroll to  view page details</h2>
-					<h3 style="display:none;">View page details</h3>
+					<!--<h1 v-if="locale=='en-ca'">{{currentPage.title}}</h1>-->
+					<!--<h1 v-else>{{currentPage.title_2}}</h1>-->
+					<!--<h2 style="display:none;">Scroll to  view page details</h2>-->
+					<!--<h3 style="display:none;">View page details</h3>-->
 				</div>
 			</div>
 		</div>
