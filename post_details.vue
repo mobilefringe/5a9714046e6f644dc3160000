ec003d9455ff1fe3497bd60ba4f46466571b72d6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-16 16:34:28 -0400

    Bramalea City Centre 1.2  /post_details.vue

--- post_details.vue
+++ post_details.vue
@@ -1,41 +1,30 @@
 <template>
     <div v-if="dataloaded">
-        <!-- v-if="pageBanner"  -->
-        <div class="page_header" v-bind:style="{ backgroundImage: 'url(http://via.placeholder.com/1920x300)' }"> <!-- { backgroundImage: 'url(' + pageBanner.image_url + ')' } -->
-			<div class="site_container">
-				<div class="header_content">
-					<h1>{{ $t("blog_page.title") }}</h1>
-				</div>
-			</div>
-		</div>
-		<div class="site_container inside_page_content page_content">
+        <div class="margin_60"></div>
+		<div class="site_container inside_page_content page_content" v-if="currentPost">
     		<div class="row">
                 <div class="col-md-12">
                     <router-link to="/posts">
-                        <p class=""><i class="fa fa-chevron-left"></i> Back to BCC Blog</p>
+                        <p class=""><i class="fa fa-chevron-left"></i> Back to Blog</p>
                     </router-link>
                 </div>
             </div>
-            <div class="row" v-if="currentPost">
+            <div class="row">
                 <div class="col-md-12">
                     <div class="post_details_image">
                         <img :src="currentPost.image_url" :alt="currentPost.title" />
                     </div>
-                    <div class="details_share_icons hidden_phone">
-                        <a href="" target="_blank">
-                            <img class="details_icon" src="//codecloud.cdn.speedyrails.net/sites/5808c7e76e6f6414b30c0000/image/jpeg/1490794350000/fb.jpg" alt="Facebook Logo">
-                        </a>
-                        <a href="" target="_blank">
-                            <img class="details_icon" src="//codecloud.cdn.speedyrails.net/sites/5808c7e76e6f6414b30c0000/image/jpeg/1490794359000/tw.jpg" alt="Twitter Logo">
-                        </a>
-                    </div>
                 </div>
-                <div class="col-md-12">
-                    <div class="blog_30 hidden_phone"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-8">
                     <div class="row_fluid">
                         <h2 class="post_details_title">{{ currentPost.title }}</h2>
                         <div class="post_details_desc" v-html="currentPost.html_body"></div>
                     </div>
+                </div>
+                <div class="col-md-4">
+                    
                 </div>
             </div>
         </div>
@@ -61,7 +50,14 @@
             watch: {
                 $route: function () {
                     this.updateCurrentBlog(this.$route.params.id);
-                }
+                },
+                currentPost: function () {
+                    if(this.currentPost != null){
+                        if (_.includes(this.currentPost.image_url, 'missing')) {
+                            this.currentPost.image_url = "http://via.placeholder.com/1200x724";
+                        }
+                    }
+                } 
             },
             computed: {
                 ...Vuex.mapGetters([
