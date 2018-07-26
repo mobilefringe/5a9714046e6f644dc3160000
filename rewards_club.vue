2ed286cdbdb139a4d8a9b2a633488b1d28f1ab59
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-07-26 09:52:01 -0400

    Bramalea City Centre 1.2  /rewards_club.vue

--- rewards_club.vue
+++ rewards_club.vue
@@ -49,7 +49,7 @@
             methods: {
                 loadData: async function() {
                     try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "	/pages/bramaleacitycentre-fashionicity.json"})]);
+                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/bramaleacitycentre-fashionicity.json"})]);
                         return results;
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
