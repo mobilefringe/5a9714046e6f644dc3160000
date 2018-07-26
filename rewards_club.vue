6c6ea519b84aaf8347f584c220a0d63f9b31fea9
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-07-26 09:52:27 -0400

    Bramalea City Centre 1.2  /rewards_club.vue

--- rewards_club.vue
+++ rewards_club.vue
@@ -3,9 +3,7 @@
 		<!-- for some reason if you do not put an outer container div this component template will not render -->
 		<div  v-if="pageBanner" class="page_header" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
 			<div class="site_container">
-				<div class="header_content">
-					<h1>{{$t("header.bees")}}</h1>
-				</div>
+				<div class="header_content"></div>
 			</div>
 		</div>
 		<div class="site_container">
