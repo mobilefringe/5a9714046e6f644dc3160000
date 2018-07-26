ac9635a2271416787ed8435514c206363f96659e
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-07-26 13:51:58 -0400

    Bramalea City Centre 1.2  /rewards_club.vue

--- rewards_club.vue
+++ rewards_club.vue
@@ -33,7 +33,7 @@
 			    </div>
 			    <div class="col-md-3">
 			        <a href="https://itunes.apple.com/ca/app/bramalea-city-centre/id1042094590?mt=8" target="_blank">
-			            <img class="rewards_badge" src="//codecloud.cdn.speedyrails.net/sites/5afb8d456e6f6436aa100000/image/png/1532627378000/applestore.png" alt="Apple App Store" />
+			            <img class="rewards_badge" src="//codecloud.cdn.speedyrails.net/sites/5afb8d456e6f6436aa100000/image/png/1532627498000/applestore.png" alt="Apple App Store" />
 			        </a>    
 			    </div>
 			    <div class="col-md-3"></div>
