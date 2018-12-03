0a423f80f2ae67376a51f1736f0b7c206eec6b33
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-03 17:08:37 -0500

    Bramalea City Centre 1.3  /header.vue

--- header.vue
+++ header.vue
@@ -13,6 +13,18 @@
     				</div>
     				<div class="col-sm-6 hidden_phone text-right">
     					<div class="header_social">
+    					<div class="search_component_container">
+                                <search-component v-if="headerReady" :list="searchList" placeholder="Search" :suggestion-attribute="suggestionAttribute" :keys="keys" v-model="search_result" @select="onOptionSelect" :autocomplete="false" :minMatchCharLength="3" :tokenize="true" class="text-left">
+                                    <template slot="item" scope="option" class="manual">
+                                        <article class="media">
+                                        <p>
+                                            {{ option.data.name }}
+                                        </p>
+                                        </article>
+                                    </template>
+                                </search-component>
+                                <!--<site-search-component site-id='www.rialtorenaissance.com' searchBoxSelector='#searchBox' themeColor='#8A251A' resultPageURL='https://dev-preview.rialtorenaissance.com/search-results' resultPageContainer="#searchResults"></site-search-component>-->
+                            </div>
     					    <span class="search_icon" @click="showSearch = !showSearch"> <i class="fa fa-search"></i></span>
     					    <social-links class="inline_block"></social-links>
     					</div>
