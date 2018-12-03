4d2bdfd3a8130dc8c740e8b613d8ed9e9770084a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-03 15:06:04 -0500

    Bramalea City Centre 1.3  /header.vue

--- header.vue
+++ header.vue
@@ -127,6 +127,7 @@
                     show_mobile_menu: false,
                     property_logo: "//codecloud.cdn.speedyrails.net/sites/5c0581a36e6f643f53050000/image/png/1527017774000/BCC-logo.png",
                     menu_items: MenuItems,
+                    showSearch: fase
                 }
             },
             watch: {
