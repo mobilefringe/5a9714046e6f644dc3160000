8937a725b91836277ce1d18f7824de61e04c716f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-03 17:35:49 -0500

    Bramalea City Centre 1.3  /header.vue

--- header.vue
+++ header.vue
@@ -13,7 +13,7 @@
     				</div>
     				<div class="col-sm-6 hidden_phone text-right">
     					<div class="header_social">
-        					<div id="search_component_wrapper" v-if="showSearch">
+        					<div id="search_component_wrapper" class="inline_block" v-if="showSearch">
         					    <div class="search_component_container" >
                                     <search-component v-if="headerReady" :list="searchList" placeholder="Search" :suggestion-attribute="suggestionAttribute" :keys="keys" v-model="search_result" @select="onOptionSelect" :autocomplete="false" :minMatchCharLength="3" :tokenize="true" class="text-left">
                                         <template slot="item" scope="option" class="manual">
