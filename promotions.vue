3062ebffc3baa44b94d1745a063ecbcd62cce6d2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-05 12:15:30 -0500

    Bramalea City Centre 1.2  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -128,7 +128,7 @@
                             temp_promo.push(value);
                         }
                     });
-                    temp_promo = _.sortBy(temp_promo, ['show_on_web_date', 'end_date']);
+                    temp_promo = _.sortBy(temp_promo, ['show_on_web_date', 'start_date']);
                     return temp_promo;
                 },
             },
