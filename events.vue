e338bbba1ada615d2da7027cf9eb1a5fbc540329
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-18 16:42:22 -0400

    Bramalea City Centre 1.4  /events.vue

--- events.vue
+++ events.vue
@@ -4,7 +4,7 @@
 			<!--http://via.placeholder.com/1920x300-->
 			<div class="site_container">
 				<div class="header_content caps">
-					<h1>News</h1>
+					<h1>{{$t("events_page.events")}}</h1>
 					<h2 style="display:none;">Scroll to view events</h2>
 					<h3 style="display:none;">View all events below</h3>
 					
