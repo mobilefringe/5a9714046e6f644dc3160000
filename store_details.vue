ba741cf14d24078a747f4be030776a6d6c291ba5
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-27 12:28:19 -0400

    Bramalea City Centre 1.4  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -38,8 +38,8 @@
 							</li>
 						</ul>
 						<div class="col-xs-12" v-if="currentStore.has_giftcard">
-						    <p class="col-xs-6 gift_card text-left">This store accepts BCC gift cards</p>
-						    <img class="col-xs-6 bitmap"src="//codecloud.cdn.speedyrails.net/sites/5c0581a36e6f643f53050000/image/png/1553632445000/CardICON@2x.png" />
+						    <p class="col-xs-5 gift_card text-left">This store accepts BCC gift cards</p>
+						    <img class="col-xs-5 bitmap"src="//codecloud.cdn.speedyrails.net/sites/5c0581a36e6f643f53050000/image/png/1553632445000/CardICON@2x.png" />
 						</div>
 					</div>
 				</div>
