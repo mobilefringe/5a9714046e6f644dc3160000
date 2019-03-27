1e71b7fac97eb57ab5988ff745600e3ae5822873
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-27 15:39:28 -0400

    Bramalea City Centre 1.4  /gift_cards.vue

--- gift_cards.vue
+++ gift_cards.vue
@@ -89,7 +89,7 @@
     }
 </style>
 <script>
-    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-meta"], function(Vue, Vuex, moment, tz, VueMoment, Meta) {
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-meta"], function(Vue, Vuex, moment, tz, VueMoment, Meta, Collapse) {
         Vue.use(Meta);
         return Vue.component("hours-component", {
             template: template, // the variable template will be injected
