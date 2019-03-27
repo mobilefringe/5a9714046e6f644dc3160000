62fccd7802141eb8b7a54feaea3176d8b77cafa5
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-27 12:24:02 -0400

    Bramalea City Centre 1.4  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -38,7 +38,7 @@
 							</li>
 						</ul>
 						<div class="col-xs-12" v-if="currentStore.has_giftcard">
-						    <p class="col-xs-6 gift_card">This store accepts BCC gift cards</p>
+						    <p class="col-xs-6 gift_card text-left">This store accepts BCC gift cards</p>
 						    <img class="col-xs-6 bitmap"src="//codecloud.cdn.speedyrails.net/sites/5c0581a36e6f643f53050000/image/png/1553632445000/CardICON@2x.png" />
 						</div>
 					</div>
