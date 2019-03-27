5bb705bebd59033ce515cb837be63892836e1d5b
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-27 13:20:02 -0400

    Bramalea City Centre 1.4  /gift_card.vue

--- gift_card.vue
+++ gift_card.vue
@@ -1,125 +1,131 @@
 <template>
-	<div v-if="dataloaded">
-		<div class="page_header" v-if="promoBanner" v-lazy:background-image="promoBanner.image_url">
-			<!--http://via.placeholder.com/1920x300-->
+    <div v-if="pageBanner"> <!-- for some reason if you do not put an outer container div this component template will not render -->
+        <div class="page_header" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
 			<div class="site_container">
 				<div class="header_content caps">
-					<h1>Looking for the Perfect Gift?</h1>
-					<h2 style="display:none;">Scroll to view events</h2>
-					<h3 style="display:none;">View all events below</h3>
+					<h1>{{$t("hours_page.hours")}}</h1>
+					<h2 style="display:none;">Scroll to  view hours</h2>
 				</div>
 			</div>
-		</div>
-		<div class="site_container page_content">
-			<div id="events_container" v-if="news.length > 0">
-				<paginate name="news" v-if="news" :list="news" class="paginate-list margin-60" :per="4">
-					<div class="row event_container" v-for="(item, index) in paginated('news')" :class="{ 'last': index === (paginated('news').length - 1) }">
-						<div class="col-sm-6 col-md-4 event_image_container">
-							<img :src="item.image_url"  class="event_image image" alt="item.title" />
-						</div>
-						<div class="col-sm-6 col-md-8 event_dets_container">
-							<h4 class="event_name caps" v-if="locale=='en-ca'">{{ item.title }}</h4>
-							<!--<h4 class="event_name caps" v-else>{{promo.name_2}}</h4>-->
-							<!--<div class="event_thick_line"></div>-->
-							<p class="event_dates">{{item.publish_date | moment("MMM DD, YYYY", timezone)}}</p>
-							<p class="event_dates">By: {{item.author}}</p>
-							<p class="event_desc" v-if="locale=='en-ca'">{{item.body_short}}</p>
-							<!--<p class="event_desc" v-else>{{promo.description_short_2}}</p>-->
-							<div class="text-right  col-sm-6" v-if="item" style="padding:0">
-    							<router-link :to="{ name: 'newsDetails', params: { id: item.slug }}" class="event_learn_more pull-left">
-    							    {{ $t("news_page.read_more") }} <i class="fa fa-angle-right" aria-hidden="true"></i>
-    						    </router-link>
-							</div>
-						</div>
-						<div class="col-sm-12">
-							<hr>
-						</div>
-					</div>
-				</paginate>
-			</div>
-			<div id="no_events" class="row" v-else>
-				<div class="col-md-12">
-					<p>{{$t("news_page.no_news_message")}}!</p>
-				</div>
-			</div>
-			<div class="row margin-60">
-				<div class="col-md-12">
-					<paginate-links for="news" :async="true" :limit="5" :show-step-links="true"></paginate-links>
-					<!--<paginate-links for="currentSelection" :async="true" :simple="{ next: 'Next »', prev: '« Back' }"></paginate-links>-->
-				</div>
-			</div>
-		</div>
-	</div>
+		</div>  
+        <div class="site_container">
+            <div class="all_hours_container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h3 class="hours_heading text-left" >{{$t("hours_page.reg_hours")}}</h3>
+                        <div id="hours_container" class="hours_container">
+                            <div class="hours_div text-left" v-for="hour in hours">
+                                <span>{{hour.day_of_week | moment("dddd", timezone)}} : </span>
+                                <span>{{hour.open_time | moment("h:mm A", timezone)}} - {{hour.close_time | moment("h:mm A", timezone)}}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12 ">
+                        <div class="padding_top_20"></div>
+                        
+                        <h3 class="hours_heading text-left">{{$t("hours_page.holiday_hours")}}</h3>
+                        <div id="holidays_hours_container" class="hours_container">
+                            BCC is open on the following Statutory Holidays 11am – 6pm <br/>
+                            <ul>
+                                <li>
+                                    New Year’s Day
+                                </li>
+                                <li>
+                                    Family Day
+                                </li>
+                                <li>
+                                    Victoria Day
+                                </li>
+                                <li>
+                                    Canada Day
+                                </li>
+                                <li>
+                                    Labour Day
+                                </li>
+                                <li>
+                                    Thanksgiving Day
+                                </li>
+                            </ul>
+                            
+                            *Not all retailers may open during stat holidays. Please verify holiday hours with individual stores.
+                        </div>
+                        
+                        <div class="padding_top_20"></div>
+                        
+                        <h3 class="hours_heading text-left">{{$t("hours_page.holiday_closures")}}</h3>
+                        <div id="closed_hours_container" class="hours_container">
+                            <div class="hours_div text-left" v-for="hour in closeHolidays">
+                                <span>
+                                    <span v-if="locale=='en-ca'">{{hour.holiday_name}}</span>
+                                    <span v-else>{{hour.holiday_name_2}}</span>
+                                    ({{hour.holiday_date | moment("MMM D YYYY", timezone)}})
+                                </span>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="padding_top_40"></div>
+            </div>
+        </div >
+    </div>
 </template>
-
+<style>
+    .hours_page_container .row{
+        margin-left:inherit;
+    }
+</style>
 <script>
-    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-meta", "vue-lazy-load", "vue-paginate", "vue-social-sharing"], function(Vue, Vuex, moment, tz, VueMoment, Meta, VueLazyload, VuePaginate, SocialSharing) {
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-meta"], function(Vue, Vuex, moment, tz, VueMoment, Meta) {
         Vue.use(Meta);
-        Vue.use(VueLazyload);
-        Vue.use(VuePaginate);
-        return Vue.component("gift-cards-component", {
+        return Vue.component("hours-component", {
             template: template, // the variable template will be injected
             props:['locale'],
             data: function() {
                 return {
-                    dataloaded: false,
-                    promoBanner: null,
-                    paginate: ['news'],
+                    pageBanner : null
                 }
             },
             created() {
                 this.loadData().then(response => {
-                    this.posts
-                    this.dataloaded = true;
-                    
-                    var temp_repo = this.findRepoByName('Pages Banner');
-                    if(temp_repo) {
-                        this.promoBanner = temp_repo.images[0];
-                    }
+                    this.pageBanner = this.findRepoByName('Hours Banner').images[0];
                 });
+                
             },
             computed: {
                 ...Vuex.mapGetters([
                     'property',
                     'timezone',
-                    'processedEvents',
-                    'findRepoByName',
-                    'news',
-                    'findBlogByName'
+                    'getPropertyHours',
+                    'getPropertyHolidayHours',
+                    'findRepoByName'
                 ]),
-                news() {
-                    var news = this.findBlogByName("News").posts;
-                    var vm = this;
-                    var temp_news = [];
-                    _.forEach(news, function(value, key) {
-                        today = moment().tz(vm.timezone);
-                        webDate = moment(value.publish_date).tz(vm.timezone);
-                        if (today >= webDate) {
-                            if (_.includes(value.image_url, 'missing')) {
-                                value.image_url = "//codecloud.cdn.speedyrails.net/sites/5c0581a36e6f643f53050000/image/jpeg/1527006352000/bccblogplaceholder.jpg";
-                            }
-                            value.body_short = _.truncate(value.body, { 'length': 99, 'separator': ' ' });
-                            
-                            temp_news.push(value);
-                        }
-                    });
-                    news = _.reverse(_.sortBy(temp_news, function (o) { return o.publish_date }));
-                    console.log(news)
-                    return news
+                hours () {
+                    return this.getPropertyHours;
                 },
+                holidayHours () {
+                    return this.getPropertyHolidayHours;
+                },
+                reducedHolidays () {
+                    var holidayHours = this.holidayHours;
+                    return _.filter(holidayHours, function(o) { return !o.is_closed; });
+                },
+                closeHolidays () {
+                    var holidayHours = this.holidayHours;
+                    return _.sortBy(_.filter(holidayHours, function(o) { return o.is_closed; }), [function(o) { return o.holiday_date; }]);
+                }
             },
-            methods: {
+            methods : {
                 loadData: async function() {
                     try {
                         // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
-                        let results = await Promise.all([this.$store.dispatch("getData", "blogs"), this.$store.dispatch("getData", "repos")]);
+                        let results = await Promise.all([this.$store.dispatch("getData", "repos")]);
+                        return results;
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
                     }
-                },
-                shareURL(slug){
-                    var share_url = "http://bramaleacitycentre.com/news/" + slug;
-                    return share_url;
                 },
             }
         });
