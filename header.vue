867189c378799d36d9b2fdfe8004cc16c68ce8ab
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-28 12:43:50 -0400

    Bramalea City Centre 1.4  /header.vue

--- header.vue
+++ header.vue
@@ -67,7 +67,7 @@
                                                         <i v-else  class="fa fa-plus"></i>
                                                     </b-btn>
                                                 </b-card-header>
-                                                <b-collapse id="store" v-model="item.show_sub_menu" :id="item.id" :visible="item.show_sub_menu" :accordion="$t(item.name)" role="tabpanel" class="accordion_body">
+                                                <b-collapse id="item.id" v-model="item.show_sub_menu" :id="item.id" :visible="item.show_sub_menu" :accordion="$t(item.name)" role="tabpanel" class="accordion_body">
                                                     <b-card-body v-for="sub_menu in item.sub_menu">
                                                         <p class="card-text"><router-link :to="sub_menu.href">{{$t(sub_menu.name)}}</router-link></p>
                                                     </b-card-body>
