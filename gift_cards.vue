1e39c63acc87f05089a2ea103f9b252997a9a6b7
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-28 11:01:44 -0400

    Bramalea City Centre 1.4  /gift_cards.vue

--- gift_cards.vue
+++ gift_cards.vue
@@ -89,9 +89,9 @@
     }
 </style>
 <script>
-    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-meta", 'bootstrap-vue'], function(Vue, Vuex, moment, tz, VueMoment, Meta, Collapse) {
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-meta", 'bootstrap-vue'], function(Vue, Vuex, moment, tz, VueMoment, Meta, BootstrapVue) {
         Vue.use(Meta);
-        Vue.use(Collapse)
+        Vue.use(BootstrapVue)
         return Vue.component("hours-component", {
             template: template, // the variable template will be injected
             props:['locale'],
