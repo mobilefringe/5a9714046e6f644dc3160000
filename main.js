00d491f5fbb78656a17a29d189bf203f84ce7435
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-08 09:26:23 -0400

    Bramalea City Centre 1.4  /main.js

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ require.config({
         'loader': 'https://mmvue.codecloudapp.com/loader.vue?noext',
         'campaignMonitor': 'https://js.createsend1.com/javascript/copypastesubscribeformlogic',
         // 'bootstrap-vue' : 'https://unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue'
-        'bootstrap-vue' : 'unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.min'
+        'bootstrap-vue' : 'https://unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.min'
     }
 });
 
