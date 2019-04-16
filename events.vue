1d237229acbf9b75c8e6beb8e9c14c3193d319ba
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-15 21:20:44 -0400

    Bramalea City Centre 1.4  /events.vue

--- events.vue
+++ events.vue
@@ -1,13 +1,11 @@
 <template>
 	<div v-if="dataloaded">
 		<div class="page_header" v-if="promoBanner" v-lazy:background-image="promoBanner.image_url">
-			<!--http://via.placeholder.com/1920x300-->
 			<div class="site_container">
 				<div class="header_content caps">
 					<h1>{{$t("events_page.events")}}</h1>
 					<h2 style="display:none;">Scroll to view events</h2>
 					<h3 style="display:none;">View all events below</h3>
-					
 				</div>
 			</div>
 		</div>
