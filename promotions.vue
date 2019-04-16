cdb181e32197b3f46176ec844477d09dcfb455c4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-04-16 17:16:24 -0400

    Bramalea City Centre 1.4  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -1,5 +1,5 @@
 <template>
-	<div v-if="dataloaded">
+	<div v-show="dataloaded">
 		<div class="page_header" v-if="promoBanner" v-lazy:background-image="promoBanner.image_url">
 			<!--http://via.placeholder.com/1920x300-->
 			<div class="site_container">
