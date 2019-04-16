0e384765c4564fc629276d36187e1b3a7ca5d5b5
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-15 21:28:26 -0400

    Bramalea City Centre 1.4  /events.vue

--- events.vue
+++ events.vue
@@ -187,7 +187,7 @@
                             temp_promo.push(value);
                         }
                     });
-                    temp_promo = _.sortBy(temp_promo, ['end_date']).reverse();
+                    temp_promo = _.sortBy(temp_promo, ['end_date']);
                     return temp_promo;
                 },
             },
