bf92eee0f30a1e65241151575ad9c74098541154
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-15 21:51:39 -0400

    Bramalea City Centre 1.4  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -12,12 +12,12 @@
 		</div>
 		<div class="site_container page_content">
 			<div id="events_container" v-if="promotions.length > 0">
-				<paginate name="promos" v-if="promos" :list="promos" class="paginate-list margin-60" :per="4">
+				<paginate name="promos" v-if="promos" :list="promos" class="paginate-list margin-60" :per="5">
 					<div class="row event_container" v-for="(promo,index) in paginated('promos')" :class="{ 'last': index === (paginated('promos').length - 1) }">
-						<div class="col-sm-6 col-md-4 event_image_container">
+						<div class="col-sm-6 col-md-3 event_image_container">
 							<img :src="promo.store.store_front_url_abs"  class="event_image image" :alt="'Click here to view ' + promo.name"/>
 						</div>
-						<div class="col-sm-6 col-md-8 event_dets_container">
+						<div class="col-sm-6 col-md-9 event_dets_container">
 							<h4 class="event_name caps" v-if="locale=='en-ca'">{{promo.name}}</h4>
 							<h4 class="event_name caps" v-else>{{promo.name_2}}</h4>
 							<div v-if="promo.promotionable_type == 'Store'">
