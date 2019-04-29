13fea201f7dfa55b9a6222dc1f01895cfdd1d4ca
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-29 15:34:11 -0400

    Bramalea City Centre 1.4  /hours.vue

--- hours.vue
+++ hours.vue
@@ -103,6 +103,7 @@
                     'timezone',
                     'getPropertyHours',
                     'getPropertyHolidayHours',
+                    'getPropertyExtendedHours',
                     'findRepoByName'
                 ]),
                 hours () {
