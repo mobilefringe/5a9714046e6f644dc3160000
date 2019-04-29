c84807bfed0d2e9f9ad6d3cee074a34f49630c50
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-29 15:56:18 -0400

    Bramalea City Centre 1.4  /hours.vue

--- hours.vue
+++ hours.vue
@@ -21,8 +21,8 @@
                         </div>
                     </div>
                     <div class="col-sm-6">
-                        <h3 class="hours_heading text-left" >{{$t("hours_page.ex_hours")}}</h3>
-                        <div id="hours_container" class="hours_container">
+                        <h3 v-if="extendedHours" class="hours_heading text-left" >{{$t("hours_page.ex_hours")}}</h3>
+                        <div v-if="extendedHours" class="hours_container">
                             <div class="hours_div text-left" v-for="hour in extendedHours">
                                 <span>
                                     {{ hour.holiday_date | moment("MMM D, YYYY", timezone) }}:
