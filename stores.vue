714815ef97bfbd5c48e458eac782547957130b01
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-05-01 14:52:52 -0400

    Bramalea City Centre 1.4  /stores.vue

--- stores.vue
+++ stores.vue
@@ -1,7 +1,6 @@
 <template>
 	<div v-if="dataloaded">
 		<div class="page_header" v-if="storeBanner" v-lazy:background-image="storeBanner.image_url">
-			<!--http://via.placeholder.com/1920x300-->
 			<div class="site_container">
 				<div class="header_content">
 					<h1>{{$t("stores_page.shopping")}}</h1>
@@ -41,7 +40,39 @@
 			</div>
 			<div class="row">
 				<div id="store_list_container">
-				    <store-masonry :filteredStores="filteredStores"></store-masonry>
+				    <!--<store-masonry :filteredStores="filteredStores"></store-masonry>-->
+				    div v-masonry transition-duration="0.3s" item-selector=".stores-grid-item" horizontal-order="true">
+        <transition-group name="custom-classes-transition" enter-active-class="animated fadeIn" leave-active-class="animated fadeOut" tag="div">
+            <div v-masonry-tile  v-for="(store, index) in filteredStores" :key="index" class="stores-grid-item">
+        	    <div class="store_logo_container">
+        	        <router-link :to="'/stores/'+ store.slug">
+            			<div v-if="!store.no_store_logo">
+            			    <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/59347e776e6f64538f150000/image/png/1545150810975/default_background.png" alt="">
+            			    <img  class="store_img" :src="store.store_front_url_abs" :alt="'Click here to view info about ' + store.name +  store.id"/>
+            			</div>
+                        <div v-else class="no_logo_container">
+                            <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/59347e776e6f64538f150000/image/png/1545150810975/default_background.png" alt="">
+                            <div class="no_logo_text">
+                                <div class="store_text"><h2>{{ store.name }}</h2></div>
+                            </div>
+                        </div>
+            			<div class="store_tag" v-if="store.total_published_promos">
+							<div class="store_tag_text">Promotion</div>
+						</div>
+						<div class="store_tag" v-if="!store.total_published_promos && store.is_coming_soon_store">
+							<div class="store_tag_text">Coming Soon</div>
+						</div>
+						<div class="store_tag" v-if="!store.total_published_promos && !store.is_coming_soon_store && store.is_new_store">
+							<div class="store_tag_text">New Store</div>
+						</div>
+						<div class="store_details">
+						    <div class="store_text"><h2>{{ store.name }}</h2></div>    
+						</div>
+            		</router-link>
+        	    </div>
+            </div>
+        </transition-group>
+    </div>
 				</div>
 			</div>
 		</div>
