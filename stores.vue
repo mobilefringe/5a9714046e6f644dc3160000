eb6408eba8e85782ce06dab3ba1ff2f1dee0d214
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-05-01 15:05:47 -0400

    Bramalea City Centre 1.4  /stores.vue

--- stores.vue
+++ stores.vue
@@ -77,9 +77,7 @@
 			</div>
 			<div class="row">
 			    <div class="col-md-12 text-center">
-			        <div class="load_more contact_btn" v-if="filteredStores && showMore <= filteredStores.length" @click ="loadMore()">
-                        <p>Load More</p>
-                    </div>
+			        <div class="load_more contact_btn" v-if="filteredStores && showMore <= filteredStores.length" @click ="loadMore()">Load More</div>
                 </div>
             </div>
 		</div>
