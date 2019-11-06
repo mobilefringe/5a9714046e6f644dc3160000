44284b60c0161a35da44a68cb06d425f803d879c
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-06 14:24:09 -0500

    Bramalea City Centre 1.4  /home.vue

--- home.vue
+++ home.vue
@@ -36,7 +36,7 @@
 </template>
 
 <script>
-define(["Vue", "vuex", "vue-meta", "vue-meta", "vue!today_hours", "vue!search-component", 'vue!vue-slick', 'js-cookie', 'vue-lazy-load', "vue!feature_masonry"], function(Vue, Vuex, Meta, TodayHoursComponent, SearchComponent, slick, Cookies, VueLazyload, featureMasonry) {
+define(["Vue", "vuex", "vue-meta", "vue!today_hours", "vue!search-component", 'vue!vue-slick', 'js-cookie', 'vue-lazy-load', "vue!feature_masonry"], function(Vue, Vuex, Meta, TodayHoursComponent, SearchComponent, slick, Cookies, VueLazyload, featureMasonry) {
     
     // define(["Vue", "vuex", "vue-meta", "vue!today_hours", "vue!search-component", 'vue!vue-slick', 'js-cookie', 'vue-lazy-load', "vue!masonry_component"], function(Vue, Vuex, Meta, TodayHoursComponent, SearchComponent, slick, Cookies, VueLazyload, VueMasonryComponent) {
         Vue.use(VueLazyload);
@@ -66,8 +66,7 @@ define(["Vue", "vuex", "vue-meta", "vue-meta", "vue!today_hours", "vue!search-co
                     meta: {
                         meta_title: "",
                         meta_description: "",
-                        meta_keywords: "",
-                        meta_image: ""
+                        meta_keywords: ""
                     }
                 }
             },
@@ -211,16 +210,13 @@ define(["Vue", "vuex", "vue-meta", "vue-meta", "vue!today_hours", "vue!search-co
                 }
             },
             metaInfo () {
-               return {
-                  title: this.meta.meta_title,
-                  meta: [
-                     { name: 'description', vmid: 'description', content: this.meta.meta_description },
-                     { name: 'keywords',  vmid: 'keywords', content: this.meta.meta_keywords },
-                     { property: 'og:title', vmid: 'og:title', content: this.meta.meta_title },
-                     { property: 'og:description', vmid: 'og:description', content: this.meta.meta_description },
-                     { property: 'og:image', vmid: 'og:image', content: this.meta.meta_image }
-                  ]
-               }
+                return {
+                    title: this.meta.meta_title,
+                    meta: [
+                        {name: 'description', content: this.meta.meta_description},
+                        {name: 'keywords', content: this.meta.meta_keywords}
+                    ] 
+                }
             }
         })
     })
