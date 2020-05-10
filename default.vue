4ae55365fddf3482842938fcc1aba5fc86d0daaf
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2020-05-09 22:27:10 -0400

    Bramalea City Centre 1.4  /default.vue

--- default.vue
+++ default.vue
@@ -3,10 +3,28 @@
 </template>
 
 <script>
-  define(["Vue", "vue-meta"], function(Vue, Meta) {
+  define(["Vue"], function(Vue) {
     return Vue.component("default-component", {
       template: template, // the variable template will be injected
       props:['locale'],
+       head: {
+            // To use "this" in the component, it is necessary to return the object through a function
+            title: function () {
+              return {
+                inner: this.meta.meta_title,
+                separator: ' ', // Leave empty separator
+                complement: ' ' // Leave empty complement
+              }
+            },
+            meta: function () {
+              return [
+                 { name: 'description', id: 'description', content: this.meta.meta_description },
+                 { name: 'keywords',  id: 'keywords', content: this.meta.meta_keywords },
+                 { property: 'og:title', id: 'og:title', content: this.meta.meta_title },
+                 { property: 'og:description', id: 'og:description', content: this.meta.meta_description }
+              ]
+            }
+        },
       data: function() {
         return {
           meta: {
@@ -21,10 +39,13 @@
         next(vm => {
           // access to component instance via `vm`
           vm.meta = vm.findMetaDataByPath(to.path);
+          vm.$emit('updateHead')
+          console.log('vm.meta', vm.meta, 'to.path', to.path)
         })
       },
       beforeRouteUpdate (to, from, next) {
         this.meta = this.findMetaDataByPath(to.path);
+        this.$emit('updateHead')
         next();
       },
       computed: {
@@ -32,15 +53,18 @@
           return this.$store.getters.findMetaDataByPath;
         }
       },
-      metaInfo () {
-        return {
-          title: this.meta.meta_title,
-          meta: [
-            {name: 'description', content: this.meta.meta_description},
-            {name: 'keywords', content: this.meta.meta_keywords}
-          ]
-        }
-      }
+        // metaInfo () {
+        //   return {
+        //       title: this.meta.meta_title,
+        //       meta: [
+        //          { name: 'description', vmid: 'description', content: this.meta.meta_description },
+        //          { name: 'keywords',  vmid: 'keywords', content: this.meta.meta_keywords },
+        //          { property: 'og:title', vmid: 'og:title', content: this.meta.meta_title },
+        //          { property: 'og:description', vmid: 'og:description', content: this.meta.meta_description },
+        //          { property: 'og:image', vmid: 'og:image', content: this.meta.meta_image }
+        //       ]
+        //   }
+        // }
     });
   });
 </script>
