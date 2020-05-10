22f0cc04b517b624911389cef3bc99bd923acbd8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2020-05-09 22:34:43 -0400

    Bramalea City Centre 1.4  /hours.vue

--- hours.vue
+++ hours.vue
@@ -72,8 +72,7 @@
     }
 </style>
 <script>
-    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment"], function(Vue, Vuex, moment, tz, VueMoment, Meta) {
-        Vue.use(Meta);
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment"], function(Vue, Vuex, moment, tz, VueMoment) {
         return Vue.component("hours-component", {
             template: template, // the variable template will be injected
             props:['locale'],
